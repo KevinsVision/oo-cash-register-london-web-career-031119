f91c174e6e82045e1d037d048a35df27fab5de45
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-03-17 22:27:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,22 @@
-
+class CashRegister
+  
+  attr_accessor :total, :title, :price, :discount
+  
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+    @item = []
+  end
+  
+  def add_item(title, price, quantity = 1)
+    # for for item in price do
+      @title = title
+      @price = price
+      quantity += 1
+# end
+    # @item * price += quantity
+     
+  end
+  
+  
+end
